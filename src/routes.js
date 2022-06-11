6c9ccad02e23c0049aa6440d9ece3d3576fa7f94
Bruna Kroth <bruna_kroth@hotmail.com>
2022-06-11 15:15:04 -0300

    finalizando aula 17

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,8 @@ import Database from './database/index';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import CollaboratorController from './app/controllers/CollaboratorController';
+
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -16,6 +18,10 @@ routes.post('/session', SessionController.store);
 // Rotas autenticadas:
 routes.use(authMiddleware)
 routes.put('/users', UserController.update);
+
+// Lista todos os colaboradores:
+routes.get('/collaborator', CollaboratorController.index)
+
 
 // Upload de arquivos:
 routes.post('/files', upload.single('file'), FileController.store);
