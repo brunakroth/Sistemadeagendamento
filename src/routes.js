dc9de94163ab98052e5b669a0ee386cba3fc8899
Bruna Kroth <bruna_kroth@hotmail.com>
2022-06-11 16:45:10 -0300

    finalizando aula 18

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 import multer from 'multer';
+
 import multerConfig from './config/multer';
 import Database from './database/index';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import CollaboratorController from './app/controllers/CollaboratorController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -18,6 +21,9 @@ routes.post('/session', SessionController.store);
 // Rotas autenticadas:
 routes.use(authMiddleware)
 routes.put('/users', UserController.update);
+
+// Rota de agendamento:
+routes.post('/appointments', AppointmentController.store)
 
 // Lista todos os colaboradores:
 routes.get('/collaborator', CollaboratorController.index)
